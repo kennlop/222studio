8ac29e5e87c86ac2c13da748810895e12fab106e
kennlop <kenneth.lopez115@gmail.com>
2023-12-05 01:03:37 -0500

    horizontal scroll no matter where cursor is

--- horizontalScroll.js
+++ horizontalScroll.js
@@ -1,8 +1,7 @@
-const projectshowcaserow = document.querySelector('.project-showcase-row');
-
-        projectshowcaserow.addEventListener('wheel', (event) => {
+window.addEventListener('wheel', (event) => {
+        const projectshowcaserow = document.querySelector('.project-showcase-row');
         const deltaY = event.deltaY;
         const newScrollLeft = projectshowcaserow.scrollLeft + deltaY;
-
+      
         projectshowcaserow.scrollLeft = newScrollLeft;
-});
+ });
--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-const projectsRow = document.querySelector('.projects-row');
-
-        projectsRow.addEventListener('wheel', (event) => {
+window.addEventListener('wheel', (event) => {
+        const projectsRow = document.querySelector('.projects-row');
         const deltaY = event.deltaY;
         const newScrollLeft = projectsRow.scrollLeft + deltaY;
-
+      
         projectsRow.scrollLeft = newScrollLeft;
 });
